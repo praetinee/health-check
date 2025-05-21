61f53c84aa0ea77cdcf00d597d9c386b0b3ed3e7
praetinee <praetinee@gmail.com>
2025-05-21 13:55:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -105,7 +105,7 @@ def interpret_bp(sbp, dbp):
         elif sbp < 120 and dbp < 80:
             return "ความดันโลหิตปกติ"
         else:
-            return "ความดันโลหิตปกติค่อนข้างสูง"
+            return "ความดันโลหิตค่อนข้างสูง"
     except:
         return "-"
 
