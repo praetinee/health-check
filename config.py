dc79fc206d804556d0d4cbfd54e50b80099d9f86
praetinee <praetinee@gmail.com>
2025-05-22 11:42:21 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,14 @@
+years = list(range(61, 69))
+columns_by_year = {
+    year: {
+        "weight": f"น้ำหนัก{year}" if year != 68 else "น้ำหนัก",
+        "height": f"ส่วนสูง{year}" if year != 68 else "ส่วนสูง",
+        "waist": f"รอบเอว{year}" if year != 68 else "รอบเอว",
+        "sbp": f"SBP{year}" if year != 68 else "SBP",
+        "dbp": f"DBP{year}" if year != 68 else "DBP",
+        "pulse": f"pulse{year}" if year != 68 else "pulse",
+        "bmi_value": f"BMI{year}" if year != 68 else "ดัชนีมวลกาย",
+    }
+    for year in years
+}
 
