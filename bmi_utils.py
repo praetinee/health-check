7dcdcf261838726e555794e9d124caf0cfe72ca0
praetinee <praetinee@gmail.com>
2025-05-22 11:41:39 +0700

    Update bmi_utils.py

--- bmi_utils.py
+++ bmi_utils.py
@@ -1,1 +1,33 @@
+def interpret_bmi(bmi):
+    try:
+        bmi = float(bmi)
+        if bmi > 30:
+            return "อ้วนมาก"
+        elif bmi >= 25:
+            return "อ้วน"
+        elif bmi >= 23:
+            return "น้ำหนักเกิน"
+        elif bmi >= 18.5:
+            return "ปกติ"
+        else:
+            return "ผอม"
+    except:
+        return "-"
+
+def interpret_bp(sbp, dbp):
+    try:
+        sbp = float(sbp)
+        dbp = float(dbp)
+        if sbp == 0 or dbp == 0:
+            return "-"
+        if sbp >= 160 or dbp >= 100:
+            return "ความดันสูง"
+        elif sbp >= 140 or dbp >= 90:
+            return "ความดันสูงเล็กน้อย"
+        elif sbp < 120 and dbp < 80:
+            return "ความดันปกติ"
+        else:
+            return "ความดันค่อนข้างสูง"
+    except:
+        return "-"
 
